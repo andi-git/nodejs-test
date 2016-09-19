2ce22c272232ef845fb8f6ee529c3e7ea50a4192
Andreas Ahammer <andreas.ahammer@gmail.com>
2016-09-19 22:18:13 +0200

    add inversify and a simple test for it

--- src/util/idGenerator.ts
+++ src/util/idGenerator.ts
@@ -1,15 +1,18 @@
+import {injectable} from "inversify";
+import "reflect-metadata";
+
 export class IdGenerator {
 
-    private static s4():string {
+    private static s4(): string {
         return Math.floor((1 + Math.random()) * 0x10000)
             .toString(16)
             .substring(1);
     }
 
-    public static guid():string {
-        let id:string = "";
-        let first:boolean = true;
-        for (let i:number = 0; i < 8; i++) {
+    public static guid(): string {
+        let id: string = "";
+        let first: boolean = true;
+        for (let i: number = 0; i < 8; i++) {
             if (first === false) {
                 id += '-';
             }
@@ -17,5 +20,34 @@ export class IdGenerator {
             first = false;
         }
         return id;
+    }
+}
+
+export interface IdGenerator2 {
+
+    guid(): string;
+}
+
+@injectable()
+export class IdGenerator2Basic implements IdGenerator2 {
+
+    guid(): string {
+        let id: string = "";
+        let first: boolean = true;
+        for (let i: number = 0; i < 8; i++) {
+            if (first === false) {
+                id += '-';
+            }
+            id += this.s4();
+            first = false;
+        }
+        return id;
+    }
+
+    //noinspection JSMethodCanBeStatic
+    private s4(): string {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
     }
 }
--- test/util/idGenerator.spec.ts
+++ test/util/idGenerator.spec.ts
@@ -1,11 +1,19 @@
-import {IdGenerator} from '../../src/util/idGenerator';
+import {IdGenerator, IdGenerator2, IdGenerator2Basic} from '../../src/util/idGenerator';
 import std = require('typescript-stl');
-
+import "reflect-metadata";
+import {Kernel} from "inversify";
 'use strict';
+
+let TYPES = {
+    IdGenerator2: Symbol("IdGenerator2")
+};
+var kernel = new Kernel();
+kernel.bind<IdGenerator2>(TYPES.IdGenerator2).to(IdGenerator2Basic);
+let idGenerator: IdGenerator2 = kernel.get<IdGenerator2>(TYPES.IdGenerator2);
 
 exports.testLengthOfGeneratedId = function (test) {
     test.expect(1);
-    test.equal(IdGenerator.guid().length, 39);
+    test.equal(idGenerator.guid().length, 39);
     test.done();
 };
 
@@ -14,7 +22,7 @@ exports.testUniquenessOfGeneratedIds = function (test) {
     let ids: std.HashSet<string> = new std.HashSet<string>();
     var numberOfIds = 10000;
     for (let i = 0; i < numberOfIds; i++) {
-        ids.push(IdGenerator.guid());
+        ids.push(idGenerator.guid());
     }
     test.equal(ids.size(), numberOfIds);
     test.done();
